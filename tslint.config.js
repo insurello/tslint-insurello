414db1bd0fd78f2afedace8abbb537a0e9db5d7a
Axel <axel.olsson@insurello.se>
2019-10-08 13:04:24 +0200

    Add tslint config security (#10)
    * Add tslint-config-security
    * Bump version
    * Add scripts validate configuration
    * Add circle ci config
    * Fix lint warnings
    
    Co-authored-by: Rasmus Larsson <rasmus.larsson@insurello.se>
    Co-authored-by: Frida Jakobsson <frida.jakobsson@insurello.se>

--- tslint.config.js
+++ tslint.config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  extends: ["tslint:latest", "tslint-config-prettier"],
   defaultSeverity: process.env.CI ? "error" : "warning",
+  extends: [
+    "tslint:latest",
+    "tslint-config-prettier",
+    "tslint-config-security"
+  ],
   rules: {
+    "import-blacklist": [true, "fp-ts/lib/Trace", { "fp-ts": ["trace"] }],
     "interface-name": [true, "never-prefix"],
     "interface-over-type-literal": false,
+    "max-classes-per-file": false,
+    "no-implicit-dependencies": [true, "dev"],
+    "no-submodule-imports": [true, "fp-ts", "io-ts", "io-ts-types"],
+    "no-var-requires": false,
     "object-literal-sort-keys": false,
     "variable-name": {
       options: [
@@ -12,11 +21,6 @@ module.exports = {
         "allow-leading-underscore",
         "allow-pascal-case"
       ]
-    },
-    "no-var-requires": false,
-    "max-classes-per-file": false,
-    "no-implicit-dependencies": [true, "dev"],
-    "no-submodule-imports": [true, "fp-ts", "io-ts", "io-ts-types"],
-    "import-blacklist": [true, "fp-ts/lib/Trace", { "fp-ts": ["trace"] }]
+    }
   }
 };
